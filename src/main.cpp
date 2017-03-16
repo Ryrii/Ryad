e872f906b230a1c0a4cfd3d997540a193f0841d7
Your Name <you@example.com>
2017-03-16 16:23:57 +0100

    bonjour

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,47 @@
 
 
 
+float dist(float* x, float* y){
+
+    float S = 0;
+
+	for(int i=0; i<784; i++) {
+        
+        S += (y[i]-x[i])*(y[i]-x[i]);
+    }
+
+    return S;
+}
+
+ 
+
 int main(int argc, char** argv)
 {
-    if(argc < 3) {fprintf(stderr, "Please provide a two mnist file\n"); exit(1); }
-    string path = argv[1];
-		string path = argv[2];	
-
-    float** data = read_mnist(path);
-	float* labels = read_labels(path_labels);	
+    float** image = read_mnist("train-images.idx3-ubyte");
+	float* labels = read_labels("train-labels.idx1-ubyte");	
+	float** test_image = read_mnist("t10k-images.idx3-ubyte");
 	
 
-    for(int i=0; i<60000; i++) {
-        printf("%u\n", i);
-        save_jpg(data[i], 28, 28, "%u/%04u.jpg", (int)labels[i], i);
+    for(int i=0; i<10000; i++) {	
+		float mind = -1;
+		int NN;
+
+		for(int j=0; j<60000; j++) {
+			float d = dist(test_image[i], image[j]);
+
+			if (d<mind || mind == -1) {
+				mind = d;
+				NN=j;
+			}
+
+		}
+
+		int inference = labels[NN]; 
+
+        save_jpg(image[i], 28, 28, "%u/%04u.jpg", (int)labels[i], i);
     }
     return 0;
 }
+ 
+
 
